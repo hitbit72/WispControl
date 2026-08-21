a30d13853f857608194488ceee7a81455aa64308
hitbit72 <hitbit2@hotmail.com>
2026-08-21 22:57:33 +0200

    url sector

--- sector/urls.py
+++ sector/urls.py
@@ -7,8 +7,8 @@ app_name = 'sectores'
 urlpatterns = [
     # Sectores
     path('', views.lista_sectores, name='lista'),
-    path('sectores/nuevo/', views.form_sector, name='nuevo_sector'),
-    path('sectores/<int:pk>/', views.detalle_sector, name='detalle_sector'),
-    path('sectores/<int:pk>/editar/', views.form_sector, name='editar_sector'),
-    path('sectores/<int:pk>/eliminar/', views.eliminar_sector, name='eliminar_sector'),
+    path('nuevo/', views.form_sector, name='nuevo_sector'),
+    path('<int:pk>/', views.detalle_sector, name='detalle_sector'),
+    path('<int:pk>/editar/', views.form_sector, name='editar_sector'),
+    path('<int:pk>/eliminar/', views.eliminar_sector, name='eliminar_sector'),
 ]
