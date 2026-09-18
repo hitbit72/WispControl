b0e9f370ecc8417a93c41f266aa0bb7923c07626
hitbit72 <hitbit2@hotmail.com>
2026-09-18 16:52:12 +0200

    Escanear APs en estaciones cliente

--- metricas/management/commands/monitorizar.py
+++ metricas/management/commands/monitorizar.py
@@ -137,8 +137,8 @@ class Command(BaseCommand):
         escalares_puerto = oids_dispositivo(dispositivo, 'puertos')
 
         # Solo los dispositivos AP y OLT
-        if dispositivo.tipo.clave == 'ap':
-            escalares_st = oids_dispositivo(dispositivo, 'wifi')
+        #if dispositivo.tipo.clave == 'ap':
+        escalares_st = oids_dispositivo(dispositivo, 'wifi')
 
         if dispositivo.tipo.clave == 'olt':
             escalares_puerto_pon = oids_dispositivo(dispositivo, 'puertos_pon')
